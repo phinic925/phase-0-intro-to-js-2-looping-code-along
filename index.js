f5a9faac04c8aca8f32211233a687479630c517d
phinic925 <phinicnyangate480@gmail.com>
2022-06-02 13:29:49 +0100

    commit tree f716c680690f7fdff828512a90c8ecd53a55dab7

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+function writeCards(array,event){
+  const message =[];
+for( let i=0; i < array.length; i++ ){
+    
+   message.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!` ) ; 
+   
+
+ }
+ return message;
+}
+
+
+function countDown(integer){
+    while (integer >= 0){
+
+    
+    console.log(integer);
+    integer--;
+    }
+
+  }
+  countDown(10);
